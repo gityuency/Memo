2d744974335b4f5282f53f6c8d2d17981cc235c0
yuency yang <ext.yuency.yang@ef.com>
2017-12-26 18:14:29 +0800

    说了很多的话

---
+++ Memo/Memo_01.swift
@@ -0,0 +1,40 @@
+//
+//  Memo_01.swift
+//  Memo
+//
+//  Created by yuency on 26/12/2017.
+//  Copyright © 2017 sunny. All rights reserved.
+//
+
+//import Foundation
+
+/*
+ 不论是谁,不管说什么,都不会改变想法
+ 誰が何と言っても考えを変えません
+ No matter who, whatever it says, it will not change my idea.
+ */
+
+/*
+ 不管多贵都想买
+ No matter how expensive it is, I want to buy
+ どん何高いても買いたいです
+ */
+
+/*
+ 无论是怎样的人,都应该有一颗温柔的心.
+ どんな人でも優しい心を持っているはずだ.
+ No matter what kind of person, there should be a gentle heart.
+ */
+
+/*
+ 一到了新年, 无论哪个神社都挤满了人.
+ 正月になると、どの神社でも人がいっぱいだ。
+ when it comes to the new year, any shrine is filled with people.
+ */
+
+/*
+ 无论怎么结实的包,长期使用都会坏掉.
+ どんなに丈夫な鞄でも、長く使えば、壊れてしまうこともある。
+ No matter how the sturdy bag is, the long term use will be broken.
+ */
+
