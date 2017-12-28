baefbc3597705604da02040e31ed0b4f8c43bc34
yuency yang <ext.yuency.yang@ef.com>
2017-12-28 11:03:11 +0800

    我要开始学习闭包了

---
+++ Memo/Swift/CloserViewController/CloserViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CloserViewController.swift
+//  Memo
+//
+//  Created by yuency on 28/12/2017.
+//  Copyright © 2017 sunny. All rights reserved.
+//
+
+import UIKit
+
+class CloserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "闭包"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Memo/ViewController.swift
+++ Memo/ViewController.swift
@@ -8,20 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/// cell 重用标示
+private let cellid = "ViewController_CELL_ID"
 
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    let listArray = [
+        ["CloserViewController": "闭包"],
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellid)
 
-    
-    
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
+
+
+
+// MARK: - 实现表格的协议方法
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    //MARK: 数据源
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listArray.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellid, for: indexPath)
+        
+        //使用字典的值作为标题
+        cell.textLabel?.text = "\(indexPath.row + 1) \(Array(listArray[indexPath.row].values)[0])"
+        cell.textLabel?.textColor = UIColor(red:0.25, green:0.25, blue:0.25, alpha:1.00)
+        return cell
+    }
+    
+    //MARK: 事件
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //因为字典的 key 就是将要 push 进来的控制器的名字
+        let vcName = Array(listArray[indexPath.row].keys)[0]
+        
+        //把这个 key 转换成为类 需要加上命名空间前缀,否则不生效
+        if let cls = NSClassFromString(Bundle.main.nameSpaceStirng + "." + vcName) as? UIViewController.Type {
+            let vc = cls.init()
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
+
 
