ea985cbf58d16c4d2c84152a3c18bd53d8e6bdc1
yuency <yuency@163.com>
2018-01-31 22:15:07 +0800

    大家好,我进这个群有一段时间了,但是却没怎么说话。  这儿价格贵的商店很多,这家店还算便宜的.

--- Memo/Japanese/Memo_02.swift
+++ Memo/Japanese/Memo_02.swift
@@ -19,3 +19,17 @@
  尊敬する人は、と聞かれた場合、皆んなは何と答えるだろうか。
  When someone asked who is your most respectful person.
  */
+
+
+/*
+ 大家好,我进这个群有一段时间了,但是却没怎么说话。
+ 皆さん、こんにちは、私はこの群に入って少し時間がありますが、あまり話をしていません。
+ All rights, I've been in this group for a while, but I didn't talk much.
+ */
+
+/*
+ 这儿价格贵的商店很多,这家店还算便宜的.
+ この辺りには高い店が多いですが、この店はどちらかといえば、安い方です。
+ There are a lot of expensive shops here, this one is cheap.
+ */
+
