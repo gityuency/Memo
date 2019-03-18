9ffbbb8d7cb9d1662617e9dcde92fa3cad12b1b6
HPCL20190110 <1290660723@qq.com>
2019-03-18 17:10:16 +0800

    完成等级11第一单元的文字练习

---
+++ Memo/English/Level_11_Unit_1_job_interviews.swift
@@ -0,0 +1,726 @@
+//
+//  Level_11_Unit_1_job_interviews.swift
+//  Memo
+//
+//  Created by HPCL20190110 on 2019/3/18.
+//  Copyright © 2019 sunny. All rights reserved.
+//
+
+
+/*
+ 
+ 
+ 顺序词
+ 使用顺序词 afterward 或 subsequently 表达继一系列事件或时间点之后。
+
+ I got my engineering degree from Delft. Afterward, I began working for Smoot.
+ 
+ 我从 Delft 获得工程师学位，之后开始为 Smoot 效力。
+
+ I graduated from high school in 2004. Subsequently, I moved to England to study at Oxford.
+ 
+ 2004 年我高中毕业，后前往英国求学牛津。
+ 
+ 使用 following 搭配宾语来表达继一系列事件或时间点之后。
+ 
+ 
+ Following my promotion, I designed the Northside Professional Center.
+ 
+ 升职之后，我设计了 Northside Professional Center 中心。
+ 
+ Following that, I was recognized as Architect of the Year.
+ 
+ 在此之后，我被评为年度建筑师。
+ 
+ 
+ 使用 previously 表示在一事件之前。
+ 
+ 
+ I started here in May. Previously, I was working for a competitor.
+ 
+ 我五月在此就职，此前在一家竞争公司上班。
+ 
+ 使用 prior to + 宾语表达在一事件或时间之前发生的事情。
+ 
+ 
+ Prior to my time managing HR, I wrote training courses.
+ 
+ 在做人力管理之前，我撰写培训课程。
+ 
+ 使用currently这个词来告诉某人你现在在哪里工作。
+ 
+ 
+ I'm currently working at Dewey and Jasper Inc.
+ 
+ 我现在在 Dewey 和 Jasper 公司上班。
+ 
+ */
+
+
+
+/*
+ 
+ Prior  to graduation, I got an internship at SWG.
+ 
+ I graduated in 2005. Subsequently  , I moved to France and stayed until 2010.
+ 
+ Following  my promotion, I was sent to the factory in Paris.
+ 
+ I was just hired at the bank. Previously  , I worked at IXW.
+ 
+ Currently  , I am working as a product manager.
+ 
+ */
+
+
+/*
+ 职业词汇
+ 
+ 
+ 在谈论自己的职业时，你经常需要提到自己占据的职位。Head 是一个通用头衔，意为 director 或 manager。Head 可作名词、动词或形容词。作名词时，后接单词 of。
+ 
+ 名词
+ 
+ He's the head of quality assurance.    他是质量保障部主管。
+ 
+ 动词
+ 
+ She heads the sales department.    她领导销售部。
+ 
+ 形容词
+ 
+ He's the head teacher.    他是带头教师。
+ 
+ 
+ */
+
+
+
+/*
+ 
+ 组织或团体中处于下级的是帮助 managers 和 directors 的人，比如 assistants 和 associates。associate 的等级通常高过 assistant。
+ 
+ 
+ associate managing editor    副总编辑
+ 
+ assistant sales director    助理销售主管
+ 
+ */
+
+
+/*
+ 
+ 
+ 以下单词在谈论职业时使用。注意它们的不同形式。
+ 
+ Verb                            Noun                               Adjective
+ 
+ accomplish    完成；实现          accomplishment    完成；成绩         accomplished    完成的；精通的
+ 
+ achieve    达到；实现             achievement    达到；成就
+ 
+                                 expertise    专门知识                expert    熟练的，老练的
+ 
+ promote    使提升                promotion    提升，晋升
+ 
+ recognize    赏识，表彰           recognition    赏识，表彰             recognizable    可识别的；可认可的
+ 
+ deserve    应得，值得                                                 deserved    应得的
+ 
+ */
+
+
+
+/*
+ 
+ I graduated with an MS in accounting.
+ 
+ 我以会计硕士学位毕业。
+ 
+ 
+ He has such extensive experience.
+ 
+ 他有如此丰富的经验。
+ 
+ 
+ It's a solid background in accounting.
+ 
+ 这是一个坚实的会计背景。
+ 
+ */
+
+
+
+
+
+/*
+ 
+ 描述你的教育
+ 你可以使用下列表达方式来写你的大学教育。注意每个表达方式搭配使用的介词。
+ 
+ I graduated from Edinburgh University in 2010.
+ 
+ 2010年我在爱丁堡大学毕业。
+ 
+ I have a master's degree in business from Beijing University.
+ 
+ 我持有北京大学的商科硕士学位。
+ 
+ I'm currently working on an MBA.
+ 
+ 我现在在学工商管理学硕士课程。
+ 
+ I majored in chemistry.
+ 
+ 我的专业是化学。
+ 
+ 
+ 
+ 描述你的经历
+ 使用这些表达方式来写你的工作经历：
+ 
+ 
+ I have a solid background in product development.
+ 
+ 我在产品研发方面有扎实的背景。
+ 
+ I have extensive experience in managing big projects.
+ 
+ 我在管理大项目方面有大量的经验。
+ 
+ I was promoted to director of sales in 2010.
+ 
+ 2010年我被提升为销售总监。
+ 
+ 
+ */
+
+
+
+
+/*
+ 
+ 为工作面试写一封求职信
+ 申请工作的时候，发送resume的同时附上一封简短的cover letter。
+ 
+ 第1段
+ 称呼语后，告诉对方你要申请什么职位，以及对你的教育和经历做一个积极的陈述。
+ 
+ I am applying for the director of sales position posted on your company website. I believe my education and extensive experience make me a strong candidate.
+ 
+ 
+ 第2段
+ 接下来，总结描述你的教育和经历。
+ 
+ I have a BS in marketing from the University of Bologna and seven years of experience in sales.
+ 
+ I worked for three years at Martinique Systems. Afterward, I worked at Hall and Smith for two years. I am currently working at Beijing Accounting Inc.
+ 
+ 
+ 第3段
+ 最后，参照你的简历，感谢对方considering（考虑）你的申请并结束写信。
+ 
+ Details about my education and experience are in the attached resume. Thank you for considering me for this position. I look forward to hearing from you.
+ 
+ */
+
+
+
+/*
+ 阅读文章
+ 
+ From:
+ efstudent@efcourse.com
+ To: bmcclain@smoot.com
+ 
+ Dear Mr. McClain:
+ 
+ I am writing to apply for the director's position at Smoot Inc. I believe my extensive experience and education make me a strong candidate.
+ 
+ I graduated with a master's degree in business from the University of Paris. I subsequently worked for three years at Cristo Systems. I am currently working at Bordeaux Consulting Inc.
+ 
+ Details about my education and experience are in the attached resume. Thank you for considering my application. I hope to hear from you soon.
+ 
+ Sincerely,
+ 
+ EF student
+ */
+
+
+// 表达 I look forward to hearing from you.
+// 表达 I hope to hear from you soon.
+
+
+
+
+/*
+ 头衔和职位
+ 尽管各个公司有不同的工作头衔，当权的人通常是从manager开始。 然后头衔从总监升为CEO 或者 president。注意senior这个词增强职位的重要性。
+ 
+ 
+ John is a senior project manager.
+ 
+ John 是高级项目经理。
+ 
+ She's the production manager.
+ 
+ 她是产品经理。
+ 
+ He's the director of design.
+ 
+ 他是设计总监。
+ 
+ Martina is the vice president of engineering.
+ 
+ Martina 是工程部副总裁。
+ 
+ */
+
+
+/*
+ 
+ 很多头衔都有c在里面。c代表chief。CEO是公司里的最高级别。其他的c-头衔向CEO汇报，但是总的来说高于vice presidents。这些是最常见的头衔。
+ 
+ 
+ chief executive officer (CEO)
+ 
+ 首席执行官(CEO)
+ 
+ chief operations officer (COO)
+ 
+ 首席运营官(COO)
+ 
+ chief financial officer (CFO)
+ 
+ 首席财务官(CFO)
+ 
+ chief technology officer (CTO)
+ 
+ 首席技术官(CTO)
+ 
+ 
+ */
+
+
+
+/*
+ 语言用法
+ 
+ It's a real pleasure to meet you.
+ 
+ 真的很高兴见到你。
+ 
+ 
+ Please call me Frank.
+ 
+ 请叫我弗兰克。
+ 
+ 
+ Thank you for giving me the opportunity.
+ 
+ 感谢您给我这个机会。
+ 
+ 
+ How was your flight?
+ 
+ 你的飞机行程怎么样？
+ 
+ 
+ How's the hotel?
+ 
+ 宾馆怎么样？
+ 
+ 
+ Shall we begin?
+ 
+ 我们开始好吗？
+ 
+ nice meeting you as well.
+ 
+ */
+
+
+
+/*
+ 
+ 找到面试官
+ 第一步是找到即将面试你的人。在一个大公司里，你可能要问administrative assistant（行政助理）或者receptionist（前台）。记得要说明你是谁和为什么去那里。
+ 
+ 
+ A: Good afternoon. I'm Rob Taylor. I have an appointment with the HR director, Ms. Rifkin.
+ 
+ 下午好。我叫 Rob Taylor。我和人事主管 Rifkin 女士约好见面。
+ B: Oh, yes. She's expecting you.
+ 
+ 哦，好的。她在等你
+ 
+ 问候面试官
+ 正式地问候interviewer（面试官），并感谢这个人抽时间来面试。聆听面试官更喜欢哪种称呼的提示语。
+ 
+ 
+ A: Good afternoon, Ms. Rifkin.
+ 
+ 下午好，Rifkin女士。
+ B: Oh, please – call me Mary.
+ 
+ 哦，请—叫我玛丽。
+ 
+ 
+ A: Thank you for the opportunity to interview for design manager.
+ 
+ 感谢您给我这个机会来面试设计经理。
+ B: It's my pleasure. You're a strong candidate.
+ 
+ 这是我的荣幸。你是一个很强的候选人。
+ 
+ 
+ 闲谈
+ 做好有一段短时间闲谈的准备。如果你来自另外一个城市，可能会问你来的路途或者你对这个城市的印象。
+ 
+ 
+ A: So how do you like Chicago?
+ 
+ 那么你对芝加哥的印象如何？
+ B: I love it! Everyone's so friendly!
+ 
+ 我很喜欢它！每个人都很友好！
+ 
+ 面试官会示意该开始面试了。
+ 
+ 
+ A: Well, we only have an hour. Shall we begin?
+ 
+ 好的，我们只有一个小时。我们开始好吗？
+ B: Sounds good.
+ 
+ 好的。
+ 
+ 
+ */
+
+
+
+/*
+ 
+ 优点和缺点的形容词
+ 
+ 
+ 形容词是表达 strengths 和 weaknesses 的重要部分。使用肯定意义的类似形容词描述你认为 smart 的人。注意这些词都是同义词。
+ 
+ 
+ clever    聪明的
+ 
+ bright    伶俐的
+ 
+ sharp    敏锐的
+ 
+ 以下形容词通常也具有肯定的意义。它们用于描述渴望成功的人。
+ 
+ 
+ ambitious    有雄心的，野心勃勃的
+ 
+ determined    坚决的
+ 
+ 
+ 
+ 请看以下描述性格的形容词。注意 sensitive 既有肯定意义，也有否定意义。
+ 
+ Positive                               Negative                Positive or negative
+ 
+ understanding    体谅的，通情达理的
+ 
+                                        touchy    易怒的
+ 
+                                        sensitive    敏感的
+ 
+                                                                thin-skinned    敏感的，脸皮薄的
+ 
+ 
+ He's such a sensitive guy. He knew I was upset, so he took me out for lunch.    他非常体谅人，知道我不开心，于是带我出去吃午饭。
+ 
+ 
+ Don't be so sensitive! You made a mistake, and now you have to accept the criticism.    不要那么敏感！你犯了错，现在就得接受批评。
+ 
+ 
+ 
+ 
+ 
+ 以下是更多描述性格的形容词。同样，有些形容词既有肯定意义，也有否定意义。同时注意 thin-skinned 的反义词是 thick-skinned。
+ 
+ Positive                               Negative                               Positive and negative
+ 
+ resilient    达观的
+ 
+                                    insensitive    不顾他人感受的；反应迟钝的
+ 
+                                                                                tough    坚强的；粗暴的
+ 
+ 
+                                                                                thick-skinned    不敏感的；不知羞耻的
+ 
+ 
+ 
+ Don't worry about her. She's tough, really thick-skinned. Nothing gets to her.    不用担心她。她很坚强，不敏感，对一切都无动于衷。
+ 
+ 
+ Sometimes I think he's too tough. He's so thick-skinned, he doesn't care if he upsets people.    有时我觉得他太粗暴了。他非常不知羞耻，根本不在乎有没有让人不高兴。
+ 
+ 
+ 
+ 
+ 
+ 以下同义词总是表达否定的意义，用于描述过于自信的人。由于这些词被认为是侮辱性语言，务必谨慎使用。
+ 
+ big-headed    自负的
+ 
+ arrogant    傲慢的
+ 
+ cocky    自以为是的
+ 
+ 
+ */
+
+
+
+/*
+ 
+ 
+ 谈论你的优点
+ 面试官常常让候选人来描述他们的优点和缺点。在你陈述一个优点后，给出额外的信息作为支持。
+ 
+ 
+ I'm very proactive. I try to fix problems before they become serious.
+ 
+ 我很主动。我尝试在问题变得严重之前来解决他们。
+ 
+ I'm enthusiastic about my work. I love being in sales.
+ 
+ 我对我的工作充满热情。我喜欢销售这行。
+ 
+ My greatest strength is curiosity. I love to learn new things.
+ 
+ 我最大的优点是好奇心。我热爱学习新的事物。
+ 
+ Persistence is my greatest strength. I don't give up until I succeed.
+ 
+ 坚持是我最大的优点。不成功我绝不放弃。
+ 
+ I'm a really good communicator. I'm especially good at listening.
+ 
+ 我非常擅长沟通。我特别善于倾听。
+ 
+ 
+ 
+ 谈论你的缺点
+ 很多面试官也会问你的最大的缺点是什么。你可以描述你做得费力但是正努力地改进的方面。
+ 
+ 
+ I've had problems with public speaking, but my manager is coaching me, so I've improved quite a bit.
+ 
+ 我的演讲能力不太好，但是我的经理正在辅导我，因此我已经提高了很多。
+ 
+ My technical knowledge was a bit weak, so I started taking computer classes at night.
+ 
+ 我的技术知识有点弱，因此我已经开始在晚上去上电脑课了。
+ 
+ My team told me that I needed to be a better listener. They're helping me work on that, and I've improved a lot.    我的团队告诉我说我需要做一个更好的倾听者。他们在帮助我改善这一点，并且我已经提高了很多。
+ 
+ 
+ */
+
+
+
+/*
+ 
+ Tell me a little bit about yourself.
+ 
+ 简单说一下你自己。
+ 
+ 
+ Why do you want to leave?
+ 
+ 你为什么想离开？
+ 
+ 
+ What are some of your strengths?
+ 
+ 你的一些强项是什么？
+ 
+ 
+ What is your greatest weakness?
+ 
+ 你最大的缺点是什么？
+ 
+ 
+ Tell me about a challenge you had.
+ 
+ 说一说你曾遇到的一个挑战。
+ 
+ */
+
+
+
+/*
+ 
+ 一些常见的面试问题
+ 很多面试官开始的时候会问一个关于你的笼统的问题。利用这个机会来推销你自己。
+ 
+ 
+ Could you tell me something about yourself?
+ 
+ 你能和我们说说你自己吗？
+ 
+ Well, I've been in the clothing design industry for seven years. I'm my company's lead designer.
+ 
+ 好的，我在服装设计行业有七年了。我是我们公司的首席设计师。
+ 
+ 面试官通常想知道你为什么想离开你现在的工作。尽量不要消极。着重强调你正在面试的这个职位的好处。
+ 
+ 
+ Why do you want to leave BTL?
+ 
+ 你为什么要离开 BTL 呢？
+ 
+ I've increased sales there by 15% in the last two years. I love it there, but I'm ready now for another challenge.
+ 
+ 过去两年中在那我已经将销售提高了15%。我很喜欢那里，但是我现在准备要迎接另一个挑战。
+ 
+ 一些面试官想要一些具体的你曾面临过的挑战的例子。
+ 
+ 
+ Tell me about a difficult situation that you faced at work and what you did about it.
+ 
+ 告诉我你在工作中遇到过的一个麻烦的情形以及你是如何应对的。
+ 
+ A junior colleague was making mistakes in his data. It made our team look bad. I taught him how to use spreadsheets effectively.
+ 
+ 一个职位较低的同事在他的数据上犯错了。这让我们团队看起来很糟糕。我教了他如何有效地使用电子表格。
+ 
+ 
+ */
+
+
+/*
+ Do you have any questions for me?
+ 
+ 你有问题要问我吗？
+ 
+ 
+ I do have a few, if you don't mind.
+ 
+ 我的确有几个问题，如果您不介意的话。
+ 
+ 
+ the greatest challenge the team's facing
+ 
+ 团队面对的最大的挑战
+ 
+ 
+ What's your experience been like?
+ 
+ 您的经验是什么样的？
+ */
+
+
+
+
+
+/*
+ 
+ 问面试官问题
+ 准备一些在面试结束时问的问题。这是显示你对公司有兴趣并得到更多关于公司信息的机会。
+ 
+ 
+ What is the biggest challenge for your staff?
+ 
+ 你的员工的最大的挑战是什么？
+ 
+ What would I need to do to be successful at this position?
+ 
+ 如果要在这个职位成功的话，我需要做些什么？
+ 
+ Do you have any reservations about my qualifications?
+ 
+ 您对我的资格有什么疑问吗？
+ 
+ 你可能想和你的面试官在私人层面上建立良好关系。不要太勉强，但是可以找出你的面试官对公司的印象如何。
+ 
+ 
+ How do you feel about working at IBX?
+ 
+ 您在 IBX 工作感觉怎样？
+ 
+ */
+
+/*
+ What sort of timeline can I expect?
+ 
+ 我可以期待什么样的时间表呢？
+ 
+ 
+ let people know by the end of the week
+ 
+ 这周结束前会通知大家
+ 
+ 
+ When are you available to start?
+ 
+ 你什么时候能开始？
+ 
+ 
+ Thank you once again for coming in.
+ 
+ 再次感谢你能过来。
+ */
+
+
+
+
+/*
+
+ 结束一个工作面试
+ 你已经问了面试官很多问题。现在你要问自己一个问题：你仍然对这个工作感兴趣吗？如果还是，你应该向面试官表达这个意思。
+ 
+ 
+ After hearing your responses to my questions, I'd really like to work here. I think that I'm a good fit for this position.
+ 
+ 听完你对我的问题的回答，我将会非常喜欢在这里工作。我觉得我是非常合适这个职位的。
+ 
+ 在你走之前，你需要知道下一步是什么。感谢面试官并且要一张名片以便你写一封简短的感谢邮件，并附上正确拼写的名字和工作头衔。
+ 
+ 
+ A: So, what are the next steps in the hiring process?
+ 
+ 那么，聘用程序中的下一步是什么？
+ B: Well, I need to interview one more candidate. I should be able to tell you my decision by next Monday.
+ 
+ 嗯，我需要再面试一个候选人。到下个星期一我应该会告诉你我的决定。
+ 
+ 
+ Thank you for taking the time to interview me. I hope to talk to you again soon.
+ 
+ 感谢您抽时间来面试我。我希望我们会很快再通话。
+ 
+ 
+ Oh, and could I have one of your business cards?
+ 
+ 哦，我能要一张您的名片吗？
+ 
+ */
+
+
+/*
+ 
+ What is the biggest challenge  for your staff?
+ 
+ What would I need to do to be successful  at this position?
+ 
+ Do you have any reservations  about my qualifications?
+ 
+ How do you feel  about working at Smoot?
+ 
+ What are the next steps  in the hiring process?
+ 
+ I think that I'm a good fit  for this position.
+ 
+ */
---
+++ Memo/English/MyMemo.swift
@@ -0,0 +1,16 @@
+//
+//  MyMemo.swift
+//  Memo
+//
+//  Created by HPCL20190110 on 2019/3/18.
+//  Copyright © 2019 sunny. All rights reserved.
+//
+
+import Foundation
+
+
+// March is the best time to be in ShangHai
+
+// she is expecting you 它正在等你
+
+// resilient
