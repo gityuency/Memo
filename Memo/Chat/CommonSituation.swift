c0ecbe844e143bf81310c0dcae4d9acb87e97997
HPCL20190110 <1290660723@qq.com>
2019-04-11 11:09:37 +0800

    在HUPU的最后一次学习提交吧

---
+++ Memo/Chat/CommonSituation.swift
@@ -0,0 +1,19 @@
+//
+//  CommonSituation.swift
+//  Memo
+//
+//  Created by HPCL20190110 on 2019/4/8.
+//  Copyright © 2019 become into an evil. All rights reserved.
+//
+
+//import Foundation
+
+
+/*
+ 当女人夸我字好看的时候
+ 
+ 1、小时候语文老师挺(好看/漂亮 使用她说过的词)的，跟你还有点像。
+ 2、你笑起来比我的字还(漂亮/好看 使用她说过的词)。
+ */
+
+
