93a4d13894820f10448ccdd5365d7fb317d04f99
ChickenMaster <1290660723@qq.com>
2019-04-28 21:35:42 +0800

    新增学习聊天记录

---
+++ Memo/Chat/和聪聪的聊天记录.swift
@@ -0,0 +1,48 @@
+//
+//  WeChatOperation.swift
+//  Memo
+//
+//  Created by HPCL20190110 on 2019/4/8.
+//  Copyright © 2019 become into an evil. All rights reserved.
+//
+
+//import Foundation
+
+
+// 这是不是刚开始认识呢
+
+/*
+
+ F: 我已经通过你的朋友验证请求,现在可以开始聊天了
+ 
+ 
+ M: ❌ 你好
+    ✅ 哈喽 ()
+    ❌ 哈喽,美女 (这个回复不是最正确的哦)
+ 
+ 
+ F: 可爱的表情包
+ 
+ 
+ M:
+ 
+ 
+ 
+ 
+ */
+
+
+/*
+ 
+ 
+ 
+ M: ✅
+ ❌  ()
+ ❌  ()
+ 
+ */
+
+
+
+
+
---
+++ Memo/Chat/导师聊天记录_1.swift
@@ -0,0 +1,34 @@
+//
+//  WeChatOperation.swift
+//  Memo
+//
+//  Created by HPCL20190110 on 2019/4/8.
+//  Copyright © 2019 become into an evil. All rights reserved.
+//
+
+//import Foundation
+
+
+// 这是不是刚开始认识呢
+
+/*
+ 
+ 
+ 
+ M: 想见你了撒
+ 
+ F: [语音]
+ 
+ M: 我也是在床上    (交流,描述自己的状态)
+    你昨晚是嗨多晚哦
+ 
+ F: 我昨天晚上出去培训了
+    我昨天晚上没有出去玩
+ 
+ 
+ M: 辛苦的哇  (口头上的安慰)
+    [送花表情]
+ 
+ 
+ */
+
