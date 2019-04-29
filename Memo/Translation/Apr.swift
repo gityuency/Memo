d0dc69829a8d8a5d18b92a7ade422eb71a69fc36
EF <EF@ef.com>
2019-04-29 09:20:31 +0800

    _ 英语翻译

---
+++ Memo/Translation/Apr.swift
@@ -0,0 +1,47 @@
+//
+//  Apr.swift
+//  Memo
+//
+//  Created by EF on 2019/4/26.
+//  Copyright © 2019 become into an evil. All rights reserved.
+//
+
+
+
+/*
+ 
+ 2019/4/28
+ 
+ We live in the richest, most prosperous, freest time in human history. That’s a wonderful gift we’ve all been given. But we’ve also shed gifts that are ancient and vital: the gift of solid family structure; the gift of common religious commitment; the gift of a culture and heritage of meritocracy（精英教育）.No government program can fill those gaps and every effort to do so ends with the exacerbation of precisely the failures government programs are designed to alleviate.
+ 
+ 我的翻译:
+ 我们处在一个人类历史上最富有,最充足,最自由的时代.这是赠与给我们美好的礼物.但是我们为这些现实的礼物感到遗憾: 坚实的家庭基础, 普通的宗教信仰, 文化和精英教育. 没有政府组织可以填满这些空缺,每一步的努力都是为了终止这样的不平衡.
+ 
+ 答案:
+ 我们生活在人类历史上最富裕、最繁荣、最自由的时代。这是给予我们所有人的一份美好的礼物。但是，我们也丢失了一些古老而重要的礼物：稳固的家庭结构；共用的宗教信仰；精英教育的文化和传统。没有任何政府项目可以填补这些空白―而且政府计划为减轻这些问题所做的所有努力，恰恰都以失败的不断加剧而告终。
+ 
+ */
+
+
+
+
+
+/*
+ 
+ 2019/4/26
+ 
+ Globalization places us in increasingly more situations where we may need to host international business associates for meals. With so many cultural differences in dining preferences, even among those
+ of the same nationality and culture, the safest and most considerate thing we can do when treating a guest is to simply ask. Avoid making too many assumptions about what your guests would like and inquire
+ about their preferences.
+
+ 
+ 我的翻译:
+ 
+ 全球化将我们放到了一个需要越来越多的主持国际商务会餐的环境,饮食文化各不相同,即使是在那些近乎相同的文化里,最安全可靠的事情是我们招待客人只需要简单的询问, 避免做太多关于客户喜好和要求的准备工作.
+
+ 标准答案:
+ 
+ 全球化可能使我们需要款待国际商业伙伴的情况日益增加。饮食喜好方面的文化差异很大，即使是在相同国籍和文化背景下，当在招待客人时，我们能够做得最安全、最周到的事情就是直接询问他们。对于你的客人喜欢什么，不要进行太多的假设，而是要去询问他们的喜好。
+
+ */
+
