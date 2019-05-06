e70c6f37bfdc3105e2e3154de3c203bd6006a861
EF <EF@ef.com>
2019-05-06 09:59:09 +0800

    _ 翻译

--- Memo/Translation/Apr.swift
+++ Memo/Translation/Apr.swift
@@ -8,6 +8,97 @@
 
 
 
+/*
+ 2019/5/5
+
+Because teenagers are adjusting to their new bodies and a whole host of new intellectual and emotional challenges, teenagers are especially self-conscious and need the confidence that comes from achieving success and knowing that their accomplishments are admired by others.
+ 
+ 
+ 我的翻译:
+ 因为青少年都调整自身面貌，情感挑战，青少年尤其关注自己并且需要通过取得成功，工作中被他人认可来获取自信。
+ 
+ 
+ 今日翻译：
+ 因为青少年正在适应新的身体状况和心智情感方面的挑战，他们的自我意识很强，需要通过取得成功以及知道别人赞赏自己的成就来获得自信。
+ */
+
+
+
+/*
+ 2019/5/4
+ 
+ Just as on smoking, voices now come from many quarters insisting that the science about global warming is incomplete, that it’s OK to keep pouring fumes into the air until we know for sure.
+ 
+ 在吸烟方面，现在很多人坚持认为现在还没有完全全球变暖，在确定之前，我们仍旧可以排放烟雾。
+ 
+ 就像吸烟一样，来自于各方的声音仍在坚持，他们认为目前关于全球气候变暖的科学研究还不充分，因此在我们确定之前，可以继续向大气中排放废气。
+ 
+ */
+
+
+
+
+/*
+ 2019/5/3
+ 
+ There’s been a steady 30-year decline in Americans’ satisfaction with the peer-to-peer relationships at work.
+ 
+ 我的翻译:
+ 美国人的一对一工作关系满意度在30年中稳步下滑
+ 
+ 答案:
+ 在美国对于工作中人际关系的满意度已经持续30年呈下降趋势。
+ 
+ */
+
+
+
+
+/*
+ 2019/5/2
+ 
+ Most parents will acknowledge that children are hard work, but are likely to say the rewards outweigh the challenges. To feel differently is almost unspeakable, but there are women who regret having children. ...Alison is keen to stress how much she loves her children but admits that in hindsight she was too selfish to have them. She says women don't talk about this for fear of being judged. "They don't want to be seen as being selfish. The implication is if you didn't want children then you're a bad mother."
+ 
+ 我的翻译:
+ 大多数的父母都知道孩子是个麻烦事，但是却不认为回报大于挑战。这些不同不可言表，但是有女人后悔生孩子。Alison 热衷于给孩子爱的压力，但是对待孩子却显得自私。她说，女人因为不想被评判而不愿提及这些。她们不想被认为自私。现实是，你不想要孩子你就是个坏妈妈。
+ 
+ 答案:
+ 大多数家长都会承认学生很辛苦，但家长更有可能会说给予学生的奖励大于他们所面对的挑战。有这样不同的感受几乎是不可言喻的，但是也有女人后悔生小孩。Alison热衷于强调她有多么热爱自己的孩子们但同时她也承认对于养小孩她过于自私。她谈论到女人会因为害怕被评判所以不谈论此话题。女人不想要被认为是自私的。如果你不想要小孩，那就意味着你是个坏母亲。
+ 
+ */
+
+
+
+/*
+ 
+ 2019/5/1
+ 
+ Darwin was convinced that the loss of these tastes for pictures or music was not only a loss of happiness, but might possibly be injurious to the intellect, and more probably to the moral character.
+ 
+ 我的翻译:
+ 绘画被认为是丧失了对图片或者音乐的品味不仅仅失去了开心，但更多是伤害了表达，更多的是道德
+ 
+ 答案:
+ 达尔文认为，对绘画或音乐失去兴趣不但使他失去了快乐，而且可能对他的智力造成损害，更可能对他的道德品质产生影响。
+ 
+ */
+
+
+
+/*
+ 
+ 2019/4/30
+ 
+ Not only may having more friends of the opposite sex be bad for grades, but it has other effects, too. Among the children from the original survey who were successfully re-interviewed 18 years later, those whose grades had been dragged down by friends of the opposite gender were more likely to be married.
+ 
+ 我的翻译:
+ 不仅拥有更多的异性朋友可能影响学习,而且也还有其他影响. 在一份最原始的儿童调查报告中, 那些18年后再次成功受访的孩子们显示,
+ 那些曾经被异性朋友把成绩拖下水的孩子们他们后来大多结婚了.
+ 
+ 答案:
+ 交更多的异性朋友不仅不利于学业，还有其他一些影响。18年后，对初次参加调查的孩子又进行了一次采访，结果显示，因交异性朋友而导致成绩下降的孩子更可能结婚。
+ 
+ */
 
 
 /*
