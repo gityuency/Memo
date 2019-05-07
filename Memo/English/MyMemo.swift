437f6aa5c8fcad8c02ca7c216e914c618d0fecc0
ChickenMaster <1290660723@qq.com>
2019-05-07 21:05:18 +0800

    提交一堆的学习

--- Memo/English/MyMemo.swift
+++ Memo/English/MyMemo.swift
@@ -14,3 +14,221 @@ import Foundation
 // she is expecting you 它正在等你
 
 // resilient
+
+
+
+/*
+ 
+ 
+ I intend to spend two days in Cape Town.
+ 我打算去开普敦呆两天。
+
+ I've made up my mind. I'm going to Rome.
+ 我想好了。我要去罗马。
+ 
+ 
+ I've finally made my decision. It's Paris.
+ 我终于下定决心了。去巴黎。
+ 
+ 
+ 
+ My itinerary is still rather rough.
+ 我的行程安排还比较粗略。
+ 
+ 
+ I'm hoping to go to Jakarta.
+ 我希望去雅加达。
+ 
+ 
+ I'm planning to visit Bergen, Norway.
+ 我正计划去挪威的卑尔根看看。
+ 
+ 
+ 
+ I'm thinking about going to Europe.
+ 我正在考虑去欧洲。
+ 
+ 
+ I really like your plan, but there's one problem.
+ 我真的很喜欢你的计划，但是有一个问题。
+ 
+ 
+ One thing you might consider is staying longer.
+ 你可以考虑多呆一段时间。
+ 
+ It would be a good idea to make an itinerary.
+ 做一个行程安排是个好主意。
+ 
+ If I were you, I'd go in January.
+ 如果我是你的话，我会一月去。
+ 
+ I'm really not sure that's a good idea.
+ 我真的不确定那是个好主意。
+ 
+ 
+ What do you recommend?
+ 你有什么推荐的？
+ 
+ 
+ 
+ My departure date is October 30th.
+ 我的出发日期是10月30号。
+ 
+ 
+ 
+ My destination is Paris.
+ 我的目的地是巴黎。
+ 
+ 
+ I have 50,000 frequent-flyer miles.
+ 我有5万飞行常客里程数。
+ 
+ 
+ 
+ I'm trying to upgrade to business class.
+ 我正试着升级到商务舱。
+ 
+ 
+ 
+ I'd really rather have a direct flight.
+ 我真的更希望能直飞。
+ 
+ 
+ 
+ You have a three-hour layover in Rome.
+ 你在罗马有三个小时的转机时间。
+ 
+ 
+ Oceania and Eastern are affiliated airlines.
+ 大洋洲航空和东方航空是联营航空公司。
+ 
+ 
+ 
+ 
+ So you're saying that I can upgrade for two hundred and fifty dollars?
+ 那么你是说我花两百五十美元就可以升级吗？
+ 
+ 
+ 
+ Let me get this straight. There's a long layover?
+ 让我把这个弄清楚。有一个很长的转机时间吗？
+ 
+ 
+ 
+ I didn't catch that. Could you repeat it?
+ 我刚才没有听见。你能重说一下吗？
+ 
+ 
+ 
+ Did you say 13 or 30?
+ 你说的是13还是30？
+ 
+ 
+ Let me make sure that I understand.
+ 让我来确认我听懂了。
+ 
+ 
+ I'm not following you. What did you say?
+ 我没听懂你的话。你刚说了什么？
+ 
+
+ 
+ The flight has been canceled due to bad weather.
+ 由于恶劣天气的原因航班已经被取消。
+ 
+ It's been delayed because of a security threat.
+ 因为安全威胁航班已经被推迟。
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ There's a mechanical problem.
+ 有一个机械问题。
+ 
+ They're having maintenance issues.
+ 存在维修问题。
+ 
+ 
+ 
+ Your flight now departs at 6 p.m.
+ 您的航班现在是下午6点起飞。
+ 
+ 
+ 
+ I was on the flight that was just canceled.
+ 我本来要坐的是刚刚被取消的那个航班。
+ 
+ 
+ 
+ 
+ We rebooked you on the same flight tomorrow.
+ 我们已经为您重订了明天的相同的航班。
+ 
+ 
+ 
+ That won't work. I have to be there tomorrow.
+ 那不行。我一定要明天赶到那里。
+ 
+ I have a couple of options for you.
+ 您有几个选择。
+ 
+ 
+ Great. What are they?
+ 太好了。都是什么呢？
+ 
+ Let me make sure that I got that right.
+ 让我来确保我弄对了。
+ 
+ I'm sending a confirmation email now.
+ 我正在发送一封确认邮件。
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
