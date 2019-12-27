7571a9e6da881479ba4749628a7fe689704a4c03
ChickenMaster <1290660723@qq.com>
2019-12-27 20:10:23 +0800

    提交一次记录

---
+++ Memo/English/liuyue12hao .swift
@@ -0,0 +1,232 @@
+//
+//  MyMemo.swift
+//  Memo
+//
+//  Created by HPCL20190110 on 2019/3/18.
+//  Copyright © 2019 sunny. All rights reserved.
+//
+
+import Foundation
+
+
+
+/*
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ You have to consider that some people are married.
+ 你一定要考虑到有些人结婚了。
+ 
+ 
+ 
+ 
+ 
+ You have to remember that some people don't drink.
+ 你一定要记得有些人不喝酒。
+ 
+ 
+ We have to take people's schedules into account.
+ 我们一定要考虑到人们的日程安排。
+ 
+ 
+ 
+ Let's not forget that some people have families.
+ 让我们别忘了有些人有家庭。
+ 
+ We have to make allowances for vegetarians.
+ 我们一定要考虑到素食主义者的需求。
+ 
+ We need to take into consideration the schedule.
+ 我们需要把日程安排考虑进来。
+ 
+ 
+ I would have to object to a weekend activity.
+ 我不得不反对周末的活动。
+ 
+ 
+ 
+ I have a problem with places that allow smoking.
+ 我不能忍受允许抽烟的地方。
+ 
+ 
+ 
+ Can I be honest with you?
+ 我能和你实话实说吗？
+ 
+ 
+ Can I be honest with you?
+ 我能和你实话实说吗？
+ 
+ 
+ We might want to consider an outdoor activity.
+ 我们可以考虑一下一个户外活动。
+ 
+ 
+ 
+ you are  wellcome thanks for listing
+ 
+ 
+ 
+ Are you enjoying yourself?
+ 你玩得开心吗？
+ 
+ Have we met before? You look familiar.
+ 我们以前见过吗？你看上去很面熟。
+ 
+ 
+ 
+ I know her, too! It really is a small world.
+ 我也认识她。世界太小了。
+ 
+ 
+ The food here is wonderful.
+ 这的食物非常好。
+ 
+ 
+ He's with Southern. He's in marketing.
+ 他和 Southern 一起的。他在市场部工作。
+ 
+ I see you work for Red Box.
+ 我看得出你为 Red Box 工作。
+ 
+ 
+ So, what brings you here?
+ 那么，你为什么来这里呢？
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ Pardon me, but I need to freshen my drink.
+ 对不起，我需要去添些饮料。
+ 
+ We really should follow up. Can I call you?
+ 我们一定得回头联系。我给你打电话好吗？
+ 
+ 
+ 
+ It was great talking to you. Enjoy the evening.
+ 非常高兴和你聊天。享受这个晚上吧。
+ 
+ 
+ I'd love to talk more, at your convenience.
+ 我非常愿意在你方便的时候能多聊聊。
+ 
+ 
+ 
+ Here -- let me give you my business card.
+ 给－让我把我的名片给你。
+ 
+ 
+ 
+ I'm actually here to look for a new position.
+ 实际上我来这是找一个新的职位。
+ 
+ 
+ 
+ I'm exploring my options.
+ 我来看看我还有什么选择。
+ 
+ 
+ I've been reluctant to say anything until now.
+ 直到现在我一直都不愿意说些什么。
+ 
+ 
+ 
+ 
+ 
+ 
+ I've been feeling very frustrated.
+ 我一直感到很沮丧。
+ 
+ 
+ She has always been very forthcoming.
+ 她总是很愿意提供信息。
+ 
+ 
+ We've been getting the runaround.
+ 我们感到橡皮球一样被踢来踢去
+ 
+ I've been given no clear information since I arrived.
+ 自从我来到这以来没有收到清晰的信息。
+ 
+ 
+ We went to a good restaurant last night.
+ 昨晚我去了一个好饭店。
+ 
+ The food was great, and the conversation excellent.
+ 食物很棒，而且对话是极好的。
+ 
+ He's been talking about opening his own business.
+ 他一直在谈论开他自己的公司。
+ 
+ I've gotten the runaround these last three days.
+ 这最后三天我一直被搪塞。
+ 
+ 
+ 
+ 
+ I've been feeling very worried about him recently.
+ 我最近对他感到很担心。
+ 
+ 
+ I'll be frank: I'm frustrated.
+ 坦白地说：我很沮丧。
+ 
+ 
+ 
+ 
+ 
+ 
+ I almost feel as if I'm getting the runaround.
+ 我几乎感到我像皮球一样被踢来踢去。
+ 
+ 
+ 
+ People just aren't being forthcoming.
+ 人们就是不能提供信息。
+ 
+ 
+ Listen -- rest assured that I will get some answers.
+ 听我说—我保证我会得到一些答案的。
+
+ 
+ 
+ I can assure you that things will get better.
+ 我敢向你保证事情会变得更好。
+ 
+ 
+ I can guarantee that the situation will change.
+ 我敢保证情况会变的。
+ 
+ I will straighten things out.
+ 我会把事情解决的。
+ 
+ 
+ over and over again
+ 
+ I asked him what the problem was.
+ 我问他怎么了。
+ 
+ 
+ He told me that he's frustrated.
+ 他告诉我说他感到沮丧。
+ 
+ He said that he needs more information.
+ 他说他需要更多的信息。
+ 
+ He asked me to straighten things out.
+ 他让我把事情搞定。
+ 
+ 
+ */
